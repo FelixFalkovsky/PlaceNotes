a8b964cb753979333ad213a77ebd07d23d703636
FELIX <felixkautz@gmail.com>
2020-08-11 09:33:34 +0300

    first commit

---
+++ PlaceNotes/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  PlaceNotes
+//
+//  Created by Felix Falkovsky on 11.08.2020.
+//  Copyright © 2020 Felix Falkovsky. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
